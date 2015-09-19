293c00abb14711ab6cac961df046eac3b3839f0c
Vsevolod Shmyroff <v.shmyroff@ya.ru>
2015-09-19 13:25:25 +0300

    init commit

---
+++ vsesh.behavior.OneTouchZoom/vsesh.behavior.OneTouchZoom.js
@@ -0,0 +1,99 @@
+/**
+ * @fileOverview
+ * Поведение изменение масштаба одним касанием. 
+ */
+ymaps.modules.define('vsesh.behavior.OneTouchZoom', [
+	'collection.Item',
+	'behavior.storage',
+	'util.defineClass',
+	'util.math.areEqual'
+], function (provide, CollectionItem, behaviorStorage, defineClass, areEqual) {
+	var MOUSEDOWN_DELAY = 500;
+	var MOUSEDOWN_POSITIONS_DIFF = 30;
+   /**
+     * @class Поведение "изменение масштаба карты одним касанием".
+     * @name vsesh.behavior.OneTouchZoom
+     * @augments IBehavior
+     * @param {Object} [options] Опции.
+     * @param {Number} [options.sensitivity = 100] Чувствительность поведения.
+	 * На сколько пикселей пользователь должен сдвинуть указатель, чтобы измененить масштаб карты на один уровень масштаба.
+     */
+	var OneTouchBehavior = defineClass(function (options) {
+		OneTouchBehavior.superclass.constructor.call(this);
+	}, CollectionItem, {
+			enable: function () {
+				this._lastMouseTimestamp = Number(new Date());
+				this.getMap().events.add('mousedown', this._onMouseDown, this);
+				this._isEnabled = true;
+			},
+
+			disable: function () {
+				this._isEnabled = false;
+				this.getMap().events.remove('mousedown', this._onMouseDown, this);
+			},
+
+			isEnabled: function () {
+				return this._isEnabled;
+			},
+
+			_onMouseDown: function (event) {
+				var timestamp = Number(new Date());
+				var currentMouseDownPosition = event.get('position');
+				if (Math.abs(this._lastMouseTimestamp - timestamp) > MOUSEDOWN_DELAY || !areEqual(this._lastMouseDownPosition, currentMouseDownPosition, MOUSEDOWN_POSITIONS_DIFF)) {
+					this._lastMouseTimestamp = timestamp;
+				} else {
+					var map = this.getMap();
+					this._preventDefaultCallback(event);
+					this._startState = {
+						position: event.get('position'),
+						zoom: map.action.getCurrentState().zoom
+					};
+					map.events
+						.add('mousemove', this._onMouseMove, this)
+						.add('mouseup', this._onMouseUp, this)
+						.once('click', this._preventDefaultCallback, this)
+						.once('dblclick', this._preventDefaultCallback, this);
+				}
+				this._lastMouseDownPosition = currentMouseDownPosition;
+			},
+
+			_onMouseMove: function (event) {
+				var map = this.getMap();
+				var newZoom = this._startState.zoom + ((this._startState.position[1] - event.get('position')[1]) / this.options.get('sensitivity', 100));
+				var currentState = map.action.getCurrentState();
+				var oldZoom = currentState.zoom;
+				map.setGlobalPixelCenter(
+					fixedToCenter(
+						currentState.globalPixelCenter,
+						map.converter.pageToGlobal(this._lastMouseDownPosition, oldZoom),
+						Math.pow(2, newZoom - oldZoom)
+					), 
+					newZoom
+				);
+				this._preventDefaultCallback(event);
+			},
+
+			_onMouseUp: function (event) {
+				this.getMap().events
+					.remove('mousemove', this._onMouseMove, this)
+					.remove('mouseup', this._onMouseUp, this);
+				this._preventDefaultCallback(event);
+			},
+
+			_preventDefaultCallback: function (event) {
+				event.preventDefault();
+			}
+		});
+
+	function fixedToCenter(oldCenter, fixedPoint, scale) {
+		var k = (scale - 1) / scale,
+			d = [(fixedPoint[0] - oldCenter[0]) * k, (fixedPoint[1] - oldCenter[1]) * k];
+		return [
+			(oldCenter[0] + d[0]) * scale,
+			(oldCenter[1] + d[1]) * scale
+		];
+	}
+
+	behaviorStorage.add('vsesh.OneTouchZoom', OneTouchBehavior);
+	provide(OneTouchBehavior);
+});
